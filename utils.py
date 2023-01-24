555c0e3bf545eb206efec3803c2de917f7d3b8ab
Anselmos <anselmos@users.noreply.github.com>
2023-01-24 15:11:54 +0100

    Fixes issue with not creating or overriding log files.

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import logging
 import logging.config
-
+from datetime import datetime
 
 def get_logger_config(name=None):
-    logging.config.fileConfig('logging.conf')
+    logging.config.fileConfig('logging.conf', defaults={
+        "log_date": datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+    })
     return logging.getLogger(name)
